de42bd3bc5a2db95c3f551e90088243d93be35ae
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-11-28 15:55:36 +0400

    1

--- src/test/java/methods/MethodsTest.java
+++ src/test/java/methods/MethodsTest.java
@@ -1,7 +1,9 @@
 package methods;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static java.time.Duration.ofMillis;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MethodsTest {
@@ -18,18 +20,18 @@ class MethodsTest {
 
 
     }
-    @org.junit.jupiter.api.Test
+    @Test
     void notNull(){
         assertNotNull(methods.summStr(str1,str2));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void empty(){
         assertFalse(str1.equals(""));
         assertFalse(str2.equals(""));
 
     }
-    @org.junit.jupiter.api.Test
+    @Test
     void nonLatin(){
         String str = methods.summStr(str1,str2);
         assertTrue(str.matches("^[a-zA-Z0-9]+$"));
@@ -37,15 +39,22 @@ class MethodsTest {
 
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void summStr()  {
         assertEquals(resstrs,methods.summStr(str1,str2));
     }
 
-    @org.junit.jupiter.api.Test
-    void factorial() {
-        int factorial = methods.factorial();
-        int result = 120;
-        assertEquals(result,factorial);
+    @Test
+    void factorial(){
+            int actional = methods.factorial();
+            int result = 120;
+            assertEquals(result, actional);
+            }
+
+    @Test
+    void timeOut(){
+        assertTimeout(ofMillis(2), () ->{
+          methods.factorial();
+        } );
     }
 }
