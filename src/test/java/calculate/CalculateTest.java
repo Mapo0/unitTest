50620e0ff17a08012c966c59e2eb1289b01ac015
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-11-28 17:30:05 +0400

    1

---
+++ src/test/java/calculate/CalculateTest.java
@@ -0,0 +1,20 @@
+package calculate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateTest {
+
+    @Test
+    void getInt() {
+    }
+
+    @Test
+    void getOperation() {
+    }
+
+    @Test
+    void calculate() {
+    }
+}
