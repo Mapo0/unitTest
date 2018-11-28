c7ccf06509b71aae7ec86ac700260b0b7a27d365
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-11-28 16:16:22 +0400

    1

--- src/test/java/methods/MethodsTest.java
+++ src/test/java/methods/MethodsTest.java
@@ -4,58 +4,61 @@ import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static java.time.Duration.ofMillis;
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MethodsTest {
-   static String str1;
-   static String str2;
-   static String resstrs ;
+    static String str1;
+    static String str2;
+    static String resstrs;
 
-    Methods methods=new Methods();
+    Methods methods = new Methods();
+
     @BeforeAll
-    static void takeStr(){
+    static void takeStr() {
         str1 = "sad";
         str2 = "boy";
-        resstrs="sadboy";
+        resstrs = "sadboy";
 
 
     }
+
     @Test
-    void notNull(){
-        assertNotNull(methods.summStr(str1,str2));
+    void notNull() {
+        assertNotNull(methods.summStr(str1, str2));
     }
 
     @Test
-    void empty(){
+    void empty() {
         assertFalse(str1.equals(""));
         assertFalse(str2.equals(""));
 
     }
+
     @Test
-    void nonLatin(){
-        String str = methods.summStr(str1,str2);
+    void nonLatin() {
+        String str = methods.summStr(str1, str2);
         assertTrue(str.matches("^[a-zA-Z0-9]+$"));
     }
 
 
-
     @Test
-    void summStr()  {
-        assertEquals(resstrs,methods.summStr(str1,str2));
+    void summStr() {
+        assertEquals(resstrs, methods.summStr(str1, str2));
     }
 
     @Test
-    void factorial(){
-            int actional = methods.factorial();
-            int result = 120;
-            assertEquals(result, actional);
-            }
+    void factorial() {
+        int actional = methods.factorial();
+        int result = 120;
+        assertEquals(result, actional);
+    }
 
     @Ignore
-    void timeOut(){
-        assertTimeout(ofMillis(2), () ->{
-          methods.factorial();
-        } );
+    void timeOut() {
+       assertTimeout(Duration.ofMillis(2), () -> {
+            methods.factorial();
+        });
     }
 }
