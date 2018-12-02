5c163494506adf3aad8c10dc1dc966b9be1b244d
Mapo0 <gabdra-marat@yandex.ru>
2018-12-02 18:38:12 +0400

    1

--- src/test/java/calculate/CalculateTest.java
+++ src/test/java/calculate/CalculateTest.java
@@ -1,11 +1,14 @@
 package calculate;
 
+import com.sun.org.apache.xpath.internal.operations.Operation;
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculateTest {
+//    @Mock
+//    private Calculate calculate;
 
     Calculate calculate = new Calculate();
     @Test
@@ -31,11 +34,11 @@ public class CalculateTest {
         int result =calculate.calculate(5,3,'-') ;
         assertEquals(result, 2);
     }
-
-    @Test
-    void  getIntTest(){
-        
-
-    }
+//    @Test
+//    void  getIntOperation(){
+//        Calculate getOperation = new getOperation('+',"сложение");
+//        Mockito.when(calculate.getOperation('+')).thenReturn(true);
+//        Assert.assertEquals(expected, actual);
+//    }
 
 }
